bcbfa4556904e7f1a24fd4d45a76f9ccaf4b7df9
johnvoyage <jmyork762@gmail.com>
2018-04-28 12:42:23 -0400

    user stats reduced

--- src/App.js
+++ src/App.js
@@ -33,7 +33,6 @@ class App extends Component {
       <div>
         <TopMenu />
         <MainSegmentOne />
-        <MainSegmentTwo />
       </div>
     );
   }
--- src/Components/UserAccount/UserHomeStats.js
+++ src/Components/UserAccount/UserHomeStats.js
@@ -1,37 +1,63 @@
 import React from 'react'
 import { Statistic, Image, Icon } from 'semantic-ui-react'
+import { connect } from 'react-redux';
 
-const UserHomeStats = () => (
-  <Statistic.Group widths='four'>
+
+const UserHomeStats = (props) => {
+  console.log(props)
+  const numOfTransactions = () => {
+    // debugger
+    return props.numOfEntries.reduce((aggr, entry) => {
+      return aggr += entry.transactions.length
+    }, 0)
+  }
+
+  return (
+    <Statistic.Group widths='four'>
     <Statistic>
-      <Statistic.Value>22</Statistic.Value>
-      <Statistic.Label>Saves</Statistic.Label>
+    <Statistic.Value>5</Statistic.Value>
+    <Statistic.Label>Transactions</Statistic.Label>
     </Statistic>
 
     <Statistic>
-      <Statistic.Value text>
-        Three
-        <br />Thousand
-      </Statistic.Value>
-      <Statistic.Label>Signups</Statistic.Label>
+    <Statistic.Value text>
+    Three
+    <br />Thousand
+    </Statistic.Value>
+    <Statistic.Label>Signups</Statistic.Label>
     </Statistic>
 
     <Statistic>
-      <Statistic.Value>
-        <Icon name='plane' />
-        5
-      </Statistic.Value>
-      <Statistic.Label>Flights</Statistic.Label>
+    <Statistic.Value>
+    <Icon name='money' />
+      $5555
+    </Statistic.Value>
+    <Statistic.Label>Net Worth</Statistic.Label>
     </Statistic>
 
     <Statistic>
-      <Statistic.Value>
-        <Image src='/assets/images/avatar/small/joe.jpg' className='circular inline' />
-        42
-      </Statistic.Value>
-      <Statistic.Label>Team Members</Statistic.Label>
+    <Statistic.Value>
+    <Image src='/assets/images/avatar/small/joe.jpg' className='circular inline' />
+    42
+    </Statistic.Value>
+    <Statistic.Label>Team Members</Statistic.Label>
     </Statistic>
-  </Statistic.Group>
-)
+    </Statistic.Group>
+  )
+}
 
-export default UserHomeStats
+
+const mapStateToProps = (state) => {
+  return {
+    // numOfTransactions:
+    numOfEntries: state.userInfo.entries,
+    numOfAccounts: state.userInfo.accounts
+
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  // mapDispatchToProps
+  null
+)(UserHomeStats)
