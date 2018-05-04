23567453de530f02d24383ed29169a06bf245caa
johnvoyage <jmyork762@gmail.com>
2018-05-04 09:37:50 -0400

    charting...

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -4,15 +4,15 @@ import GraphOptions from "./GraphOptions";
 import TimingFilter from "./TimingFilter";
 import { Segment } from "semantic-ui-react";
 
-import data from "./Data";
+import data from "../../StaticOptions/chartAesthetics";
 import { connect } from "react-redux";
 
 const Statistics = props => {
   // data: [65, 59, 80, 81, 56, 55, 40]
   // labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
 
-  data.datasets[0].data = [65, 59, 80, 81, 56, 55, 40];
-  data.labels = [
+  data.standard.datasets[0].data = [65, 59, 80, 81, 56, 55, 40];
+  data.standard.labels = [
     "January",
     "February",
     "March",
@@ -30,7 +30,7 @@ const Statistics = props => {
       <br />
       <Segment>
         <Line
-          data={data}
+          data={data.standard}
           // width={100}
           // height={50}
           // options={{
